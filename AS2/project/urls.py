c9d04b1e33aec186db35383ac2d10dda3834c2f7
Maizie <23FTT1833@student.pb.edu.bn>
2025-05-01 09:55:57 +0800

    add functions for login

--- AS2/project/urls.py
+++ AS2/project/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('viewallcars/', views.viewallcars, name='viewallcars'),
     path('profile/', views.profile, name='profile'), 
     path('sorento/', views.sorento, name='sorento'),
+    path('login/', views.login_view, name='login'),
 ]
--- AS2/project/views.py
+++ AS2/project/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'project/index.html')
-
 def viewallcars(request):
     return render(request, 'project/viewallcars.html')
 def profile(request):
     return render(request, 'project/profile.html')
 def sorento(request):
     return render(request, 'project/sorento.html')
+def login_view(request):
+    return render(request, 'project/login.html')
+
