98f01f5b7e9f6b13bf114547a07d87e7b5005c58
Maizie <23FTT1833@student.pb.edu.bn>
2025-05-10 02:53:08 +0800

    Renamed TEST SERVER to TEST_SERVER

---
+++ TEST_SERVER/Beta/Beta/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('search/', views.search_page, name='search'),
+    path('about/', views.about_page, name='about'),
+    path('category/', views.category_page, name='category'),
+    path('result/', views.search_results, name='search_results'),
+    path('sorento/', views.sorento_view, name='sorento'),
+    path('ViewAllCar/', views.ViewAllCar, name='ViewAllCar'),
+    path('', views.homepage, name='homepage'),
+    path('car/<int:car_id>/', views.car_detail, name='car_detail'),
+    path('coupe/', views.coupe, name='coupe_category'),
+    path('luxury/', views.luxury, name='luxury_category'),
+    path('SUV/', views.suv_category, name='SUV_category'),
+    path('MPV/', views.MPV, name='MPV_category'),
+    path('sport/', views.sport_category, name='sport_category'),
+    path('sedan/', views.sedan, name='sedan_category'),
+    path('hatchback/', views.hb, name='hatchback_category'),
+    path('profile/', views.profile, name='profile'),
+    path('recovery/', views.Recovery, name='ewcoveryPW'),
+    path('Create/', views.NewAcc, name='newAcc'),
+
+    path('browse/brand/', views.browse_by_brand, name='browse_by_brand'),
+    path('browse/price/', views.browse_by_price, name='browse_by_price'),
+    path('browse/model/', views.browse_by_model, name='browse_by_model'),
+    path('browse/seater/', views.browse_by_seat, name='browse_by_seat'),
+
+    path('Login/', views.Login, name='login'),
+
+   
+
+    
+    
+
+]
